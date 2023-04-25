75f0da8bae2c91131ceeec4e25e40856b7f36a32
avallete <andrew@snaplet.dev>
2023-04-25 17:22:14 +0100

    fix: query does not depends on static

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -7,16 +7,19 @@ export const appRouter = trpc
   .router()
   .query("get-pokemon-pair", {
     async resolve() {
-      const [first, second] = getOptionsForVote();
-
-      const bothPokemon = await prisma.pokemon.findMany({
-        where: { id: { in: [first, second] } },
+      const pokemonCount = await prisma.pokemon.count();
+      const skipOne = Math.floor(Math.random() * pokemonCount - 1);
+      const skipTwo = Math.floor(Math.random() * pokemonCount - 1);
+      const pokemonOne = await prisma.pokemon.findFirst({
+        take: 1,
+        skip: skipOne,
+      });
+      const pokemonTwo = await prisma.pokemon.findFirst({
+        take: 1,
+        skip: skipTwo,
       });
 
-      if (bothPokemon.length !== 2)
-        throw new Error("Failed to find two pokemon");
-
-      return { firstPokemon: bothPokemon[0], secondPokemon: bothPokemon[1] };
+      return { firstPokemon: pokemonOne, secondPokemon: pokemonTwo };
     },
   })
   .mutation("cast-vote", {
